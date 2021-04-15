1332760e13b2a53d1e9e5cc1a0526d590f29fa55
kato-masaki <0913ritz@gmail.com>
2021-04-15 12:27:49 +0000

    [Add] フォロー/フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
-  # get 'users/show'
-  # get 'users/edit'
   devise_for :users
   root 'homes#top'
   resources :diaries do
     resource :favorites, only: [:create, :destroy]
     resources :diary_comments, only: [:create, :destroy]
   end
-  
-  resources :users, only: [:show, :edit, :update]
+
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships,only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
