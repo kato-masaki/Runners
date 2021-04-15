d87c245eb47ca9d03a7e6bbe8cf5af1a09b2cd29
kato-masaki <0913ritz@gmail.com>
2021-04-15 09:23:02 +0000

    [Add] ユーザー詳細ページ、プロフィール編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  # get 'users/show'
+  # get 'users/edit'
   devise_for :users
   root 'homes#top'
   resources :diaries do
     resource :favorites, only: [:create, :destroy]
     resources :diary_comments, only: [:create, :destroy]
   end
+  
+  resources :users, only: [:show, :edit, :update]
 
 end
