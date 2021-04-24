fd4eedd5ab41ce4fdc44b4009fc8032202f8c8a3
kato-masaki <0913ritz@gmail.com>
2021-04-24 04:38:13 +0000

    [Add] good機能の非同期通信化、レイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,13 @@ class User < ApplicationRecord
 
   attachment :profile_image
 
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
 
   def follow(user_id)
