e6666e95632d6b58e615f7adbfe03d4b489561c7
kato-masaki <0913ritz@gmail.com>
2021-06-16 12:35:23 +0000

    [Update] 検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def self.search_for(content)
-    User.where('name LIKE ?', '%' + content + '%')
+    where('name LIKE ?', '%' + content + '%')
   end
 
   validates :name, length: { maximum: 20, minimum: 2 }, uniqueness: true
