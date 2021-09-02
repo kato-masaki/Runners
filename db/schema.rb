06938b2cc788824e364bce31c2336fe1175dab66
kato-masaki <0913ritz@gmail.com>
2021-09-02 02:05:11 +0000

    [Update] ユーザーデータベース

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_072719) do
+ActiveRecord::Schema.define(version: 2021_09_02_020216) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_072719) do
     t.string "profile_image_id"
     t.text "introduction"
     t.string "residence"
+    t.string "occupation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
