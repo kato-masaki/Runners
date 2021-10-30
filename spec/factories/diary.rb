4384b4ebc192c58383fd3f5ba9a772f91666660e
kato-masaki <0913ritz@gmail.com>
2021-10-30 11:21:46 +0000

    [Update] テスト

---
+++ spec/factories/diary.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :diary do
+    caption { Faker::Lorem.characters(number:30) }
+    distance { Faker::Number.number(2) }
+    hour { Faker::Number.number(2) }
+    minute { Faker::Number.number(2) }
+    second { Faker::Number.number(2) }
+    pace_minute { Faker::Number.number(2) }
+    pace_second { Faker::Number.number(2) }
+  end
+end
