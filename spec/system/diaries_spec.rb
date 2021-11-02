db77e52060b0a7a37064c605b5e3523ced44e1c8
kato-masaki <0913ritz@gmail.com>
2021-11-02 08:30:04 +0000

    [Update] テスト

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -2,3 +2,23 @@
 
 require 'rails_helper'
 
+describe '投稿のテスト' do
+  describe '投稿画面(diaries_new_path)のテスト' do
+    before do
+      visit diaries_new_path
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill _in 'diary[caption]', with: Faker::Lorem.characters(number:30)
+        fill _in 'diary[distance]', with: Faker::Number.number(2)
+        fill _in 'diary[hour]', with: Faker::Number.number(2)
+        fill _in 'diary[minute]', with: Faker::Number.number(2)
+        fill _in 'diary[second]', with: Faker::Number.number(2)
+        fill _in 'diary[pace_minute]', with: Faker::Number.number(2)
+        fill _in 'diary[pace_second]', with: Faker::Number.number(2)
+        click_button '投稿'
+        expect(page).to have_current_path diaries_path
+      end
+    end
+  end
+end
